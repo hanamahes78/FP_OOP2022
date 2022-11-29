ae42e7ffd65bf8640fe3c4923798198d88f8442d
hanamahes78 <hanamaheswari12@gmail.com>
2022-11-29 17:26:21 +0700

    Initial project

---
+++ Connect4/src/connect4/Board.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connect4;
+
+/**
+ *
+ * @author Hana Maheswari
+ */
+public class Board {
+    private String color;
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
