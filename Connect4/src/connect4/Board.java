274bfe58220a3ef0d2edf860ef089b0afd4fa30c
hanamahes78 <hanamaheswari12@gmail.com>
2022-12-03 19:43:07 +0700

    add Board, Piece class

--- Connect4/src/connect4/Board.java
+++ Connect4/src/connect4/Board.java
@@ -9,20 +9,32 @@ package connect4;
  * @author Hana Maheswari
  */
 public class Board {
-    private String color;
-
-    /**
-     * @return the color
-     */
-    public String getColor() {
-        return color;
-    }
-
-    /**
-     * @param color the color to set
-     */
-    public void setColor(String color) {
-        this.color = color;
+    private static int rows = 6;
+    private static int columns = 7;
+    
+    Piece [][]ourBoard = new Piece[rows][columns];
+    
+    public void printBoard(){
+        for(int row = 0; row < rows; row++){
+            System.out.print("|");
+            for(int col = 0; col < columns; col++){
+                if(ourBoard[row][col] == null){
+                    System.out.print("_");
+                }
+                else{
+                    System.out.print(ourBoard[row][col].getColor());
+                }
+                System.out.print("|");
+            }
+            System.out.println();
+        }
     }
     
+    public Board(){
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < columns; col++){
+                ourBoard[row][col] = null;
+            }
+        }
+    }
 }
--- Connect4/src/connect4/Connect4.java
+++ Connect4/src/connect4/Connect4.java
@@ -14,7 +14,8 @@ public class Connect4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Board boardGame = new Board();
+        boardGame.printBoard();
     }
     
 }
